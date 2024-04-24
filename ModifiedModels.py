51f16f6c01498ee268bf858287a69d647fd4e110
Pampaj <leonardo.pamaploni666@gmail.com>
2024-04-24 17:23:30 +0200

    added others models for testing. updated bearddiary to improve quality of tests

--- ModifiedModels.py
+++ ModifiedModels.py
@@ -1,10 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
-from matplotlib import pyplot as plt
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, random_split
 import torchvision.models as models
 from torchvision.models import Inception_V3_Weights
 from tqdm import tqdm
@@ -17,22 +12,22 @@ class ModifiedInceptionV3Paper(nn.Module):
         self.inception.Conv2d_1a_3x3.conv = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(2, 2), bias=False)
         self.inception.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
         self.name = "V3ModInceptionPaper"
-        #additional layers
+        # additional layers
         self.dropout = nn.Dropout(0.5)
-        self.global_average_pooling2d = nn.AvgPool2d()
+        self.global_average_pooling2d = nn.AvgPool2d(2, 2)
         self.dense = nn.Linear(2048, 256)
         self.batch_norm_layer = nn.BatchNorm1d(num_features=256)
         self.output = nn.Linear(256, num_classes)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.inception(x)
 
         x = self.dropout(x)
         x = self.global_average_pooling2d(x)
         x = self.dense(x)
-        x = self.batch_norm_layer(x) 
+        x = self.batch_norm_layer(x)
         x = self.output(x)
-    
+
 
 class ModifiedInceptionV3(nn.Module):
     def __init__(self, num_classes):
@@ -44,20 +39,60 @@ class ModifiedInceptionV3(nn.Module):
 
     def forward(self, x):
         return self.inception(x)
-    
+
 
 class ModifiedResNet(nn.Module):
     def __init__(self, num_classes):
         super(ModifiedResNet, self).__init__()
-        
+
         resnet50 = models.resnet50(pretrained=True)
         num_features = resnet50.fc.in_features
         resnet50.fc = nn.Linear(num_features, num_classes)
         for param in resnet50.parameters():
             param.requires_grad = False
-        
+
         self.model = resnet50
 
     def forward(self, x):
         return self.model(x)
 
+
+class ModifiedVGG16(nn.Module):
+    def __init__(self, num_classes):
+        super(ModifiedVGG16, self).__init__()
+        # Load the pretrained VGG16 model
+        original_model = models.vgg16(pretrained=True)
+        self.features = original_model.features
+        self.name = 'VGG16'
+
+        # Define the classifier
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.AdaptiveAvgPool2d((1, 1)),  # This is equivalent to Keras's GlobalAveragePooling2D
+            nn.Flatten(),  # Flatten the output of the pooling layer
+            nn.Linear(512, 256),  # Dense layer
+            nn.ReLU(True),
+            nn.BatchNorm1d(256),  # Batch normalization
+            nn.Dropout(0.5),  # Dropout
+            nn.Linear(256, num_classes),  # Output layer, replace num_classes with the actual number of classes
+        )
+
+    def forward(self, x):
+        x = self.features(x)  # Pass input through feature extractor
+        x = self.classifier(x)
+        return x
+
+
+class ModifiedResNet50(nn.Module):
+    def __init__(self, num_classes):
+        super(ModifiedResNet50, self).__init__()
+        original_model = models.resnet50(pretrained=True)
+        self.features = nn.Sequential(*list(original_model.children())[:-1])  # Remove last layer
+        self.classifier = nn.Linear(2048, num_classes)
+        self.name = 'ResNet50'
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.classifier(x)
+        return x
--- V3ModInception.py
+++ V3ModInception.py
@@ -9,24 +9,26 @@ import torchvision.models as models
 from torchvision.models import Inception_V3_Weights
 from tqdm import tqdm
 from ModifiedModels import *
+import json
 
 
-def chooseModel(model):
-    #
-    # 0 = ModifiedInceptionV3
-    # 1 = ModifiedInceptionV3Paper
-    # 2 = ModifiedVgg16
-    # 3 = ModifiedResnet
-    if( model == 0):
+def chooseModel(model_type):
+    if model_type == 0:
         return ModifiedInceptionV3(24)
-    elif( model == 1):
+    elif model_type == 1:
         return ModifiedInceptionV3Paper(24)
-    
+    elif model_type == 2:
+        return ModifiedVGG16(24)
+    elif model_type == 3:
+        return ModifiedResNet50(24)
+    else:
+        raise ValueError("Unknown model_type")
+
 
 def processData(batch_size):
-    #create the data loader to use during train
+    # create the data loader to use during train
     transform = transforms.Compose([
-        transforms.Resize((299, 299)),
+        transforms.Resize((299, 299)),  # TODO do padding for v3
         transforms.Grayscale(num_output_channels=3),
         transforms.RandomApply([
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
@@ -38,7 +40,6 @@ def processData(batch_size):
     ])
 
     full_dataset = ImageFolder('dataset/DataGood/ChromoClassified', transform=transform)
-    full_dataset.class_to_idx
     total_size = len(full_dataset)
     train_size = int(0.7 * total_size)
     val_size = int(0.2 * total_size)
@@ -54,8 +55,8 @@ def processData(batch_size):
 
 
 def train(model, train_loader, val_loader, test_loader, lr, epochs):
-
     device = torch.device("cuda" if torch.cuda.is_available() else "mps")
+    model.to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -75,7 +76,10 @@ def train(model, train_loader, val_loader, test_loader, lr, epochs):
         for images, labels in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch'):
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs, _ = model(images)
+            if model.name == "VGG16" or model.name == "ResNet50":  # name defined in the ModifiedModels.py
+                outputs = model(images)  # if vgg16 and resnet need 1 parameter
+            else:
+                outputs, _ = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -131,11 +135,22 @@ def train(model, train_loader, val_loader, test_loader, lr, epochs):
 
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy * 100:.2f}%')
 
-    return  train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies    
+    metrics = {
+        'train_losses': train_losses,
+        'train_accuracies': train_accuracies,
+        'validation_losses': validation_losses,
+        'validation_accuracies': validation_accuracies,
+        'test_losses': test_losses,
+        'test_accuracies': test_accuracies
+    }
+    with open(f'models/{model.name}_metrics.json', 'w') as f:
+        json.dump(metrics, f)
+
+    return train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies
 
 
-def plot(model,train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies):
-# Plotting
+def plot(model, train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies):
+    # Plotting
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
     plt.plot(train_losses, label='Training Loss')
@@ -156,16 +171,42 @@ def plot(model,train_losses, train_accuracies, validation_losses, validation_acc
     plt.legend()
 
     plt.tight_layout()
-    plt.savefig('models/' + model.name +".png")
+    plt.savefig('models/' + model.name + ".png")
     plt.show()
 
+
+def model_summary(model):
+    print("Model Summary:")
+    total_params = 0
+    for name, layer in model.named_modules():
+        # Skip the top-level module (the whole model)
+        if isinstance(layer, nn.Module) and not isinstance(layer, nn.Sequential) and layer != model:
+            params = sum(p.numel() for p in layer.parameters() if p.requires_grad)
+            print(
+                f"{name.ljust(20)} | {str(layer).ljust(30)} | Params: {params} | Trainable: {any(p.requires_grad for p in layer.parameters())}")
+            total_params += params
+    print(f"Total trainable parameters: {total_params}")
+
+
+def load_metrics(model_name):
+    with open(f'models/{model_name}_metrics.json', 'r') as f:
+        metrics = json.load(f)
+    return (metrics['train_losses'], metrics['train_accuracies'],
+            metrics['validation_losses'], metrics['validation_accuracies'],
+            metrics['test_losses'], metrics['test_accuracies'])
+
+
 def pipeline(model_type):
-    model = chooseModel(0)
-    train_loader, val_loader, test_loader = processData(batch_size = 64)
-    train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies = train(model=model, train_loader=train_loader, val_loader=val_loader, test_loader=test_loader, lr=0.0001, epochs=150)
-    plot(model,train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies)
+    model = chooseModel(model_type)
+    # model_summary(model)
+    train_loader, val_loader, test_loader = processData(batch_size=16)
+    train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies = train(
+        model=model, train_loader=train_loader, val_loader=val_loader, test_loader=test_loader, lr=0.0001, epochs=10)
+
+    plot(model, train_losses, train_accuracies, validation_losses, validation_accuracies, test_losses, test_accuracies)
 
 
 pipeline(0)
-
-
+pipeline(1)
+pipeline(2)
+pipeline(3)
